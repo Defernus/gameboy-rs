fe213e2849ba06a913af81b41464bee1be3a7744
Denis Lavrentev <dehucka98@gmail.com>
2024-07-07 07:32:18 +0300

    add window display to the runner

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -28,8 +28,37 @@ async fn main() {
     );
     let background_texture = Texture2D::from_image(&background_image);
 
+    let mut window_image = Image::gen_image_color(
+        TILE_MAP_WIDTH as u16 * TILE_WIDTH as u16,
+        TILE_MAP_HEIGHT as u16 * TILE_HEIGHT as u16,
+        MAGENTA,
+    );
+    let window_texture = Texture2D::from_image(&window_image);
+
+    let mut mode_3_duration = MODE_3_BASE_DURATION;
+
     loop {
-        draw_background_to_image(&state.emulator, &mut background_image);
+        if is_key_pressed(KeyCode::R) {
+            state.emulator = Emulator::from_rom(rom.to_vec());
+        }
+
+        if is_key_pressed(KeyCode::Up) {
+            mode_3_duration += 1;
+            state.emulator = Emulator::from_rom(rom.to_vec());
+            state.emulator.mode_3_duration = mode_3_duration;
+            println!("Mode 3 duration: {}", state.emulator.mode_3_duration);
+        }
+        if is_key_pressed(KeyCode::Down) {
+            mode_3_duration -= 1;
+            state.emulator = Emulator::from_rom(rom.to_vec());
+            state.emulator.mode_3_duration = mode_3_duration;
+            println!("Mode 3 duration: {}", state.emulator.mode_3_duration);
+        }
+
+        state.emulator.next_frame();
+
+        draw_tilemap_to_image(&state.emulator, &mut background_image, false);
+        draw_tilemap_to_image(&state.emulator, &mut window_image, true);
 
         clear_background(WHITE);
 
@@ -45,8 +74,17 @@ async fn main() {
             },
         );
 
-        state.emulator.next_frame();
-
+        window_texture.update(&window_image);
+        draw_texture_ex(
+            &window_texture,
+            256.,
+            0.,
+            WHITE,
+            DrawTextureParams {
+                dest_size: state.screen_size().into(),
+                ..Default::default()
+            },
+        );
         next_frame().await
     }
 }
